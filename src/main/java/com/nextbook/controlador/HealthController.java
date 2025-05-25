e440f3d4f274b82c42dcecef65b093c84993f8fc
Jorge Durán <j_dl@outlook.es>
2025-05-26 01:23:14 +0200

    aniadido healtcontroller

---
+++ src/main/java/com/nextbook/controlador/HealthController.java
@@ -0,0 +1,13 @@
+package com.nextbook.controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public String health() {
+        return "OK";
+    }
+}
